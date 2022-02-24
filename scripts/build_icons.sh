a105442a80d56244807c9c15ab485e8942f1232f
Adam Coddington <me@adamcoddington.net>
2022-02-24 07:09:29 -0800

    By default, use 'dev' icons.

---
+++ scripts/build_icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+convert assets/icon.png -resize 256x256 public/icon256.png 
+convert assets/icon.png -resize 128x128 public/icon128.png 
+convert assets/icon.png -resize 48x48 public/icon48.png 
+convert assets/icon.png -resize 16x16 public/icon16.png 
