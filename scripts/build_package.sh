5f5f78a126aa725f213bd1b5afda0700632b7d41
Adam Coddington <me@adamcoddington.net>
2022-02-26 12:17:33 -0800

    Updating build process for chrome.

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-export ARCHIVE="obsidian-$(jq -r '.version' package.json).zip"
+export ARCHIVE="obsidian-$(jq -r '.version' package.json).chrome.zip"
+npm run build
 cd dist/
 zip -r ../$ARCHIVE ./
